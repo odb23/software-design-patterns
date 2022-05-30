17696b56d07e619d9711df9eb44d9d95bc29cefe
odb23 <damijustice1@gmail.com>
2022-05-30 11:33:37 +0100

    completed command design pattern

--- command_pattern/script.js
+++ command_pattern/script.js
@@ -9,23 +9,19 @@
 class Calculator {
   constructor() {
     this.value = 0;
+    this.history = []
   }
 
-  add(valueToAdd) {
-    this.value = this.value + valueToAdd;
+  executeCommand (command) {
+    this.value = command.execute(this.value)
+    this.history.push(command)
   }
 
-  subtract(valueToSubtract) {
-    this.value -= valueToSubtract;
+  undo () {
+    const command = this.history.pop()
+    this.value = command.undo(this.value)
   }
 
-  divide(valueToDivide) {
-    this.value /= valueToDivide;
-  }
-
-  multiply(valueToMultiply) {
-    this.value *= valueToMultiply;
-  }
 }
 
 class AddCommand {
@@ -37,12 +33,68 @@ class AddCommand {
     return currentValue + this.valueToAdd;
   }
 
-  undo() {
+  undo(currentValue) {
     return currentValue - this.valueToAdd;
   }
 }
 
-const addCommand = new AddCommand(10);
-const newValue = addCommand.execute(10);
-console.log(newValue);
-console.log(addCommand.undo(newValue));
+class SubtractCommand {
+  constructor(valueToSubtract) {
+    this.valueToSubtract = valueToSubtract;
+  }
+
+  execute(currentValue) {
+    return currentValue - this.valueToSubtract;
+  }
+
+  undo(currentValue) {
+    return currentValue + this.valueToSubtract;
+  }
+}
+
+class MultiplyCommand {
+  constructor(valueToMultiply) {
+    this.valueToMultiply = valueToMultiply;
+  }
+
+  execute(currentValue) {
+    return currentValue * this.valueToMultiply;
+  }
+
+  undo(currentValue) {
+    return currentValue / this.valueToMultiply;
+  }
+}
+
+class DivideCommand {
+  constructor(valueToDivide) {
+    this.valueToDivide = valueToDivide;
+  }
+
+  execute(currentValue) {
+    return currentValue / this.valueToDivide;
+  }
+
+  undo(currentValue) {
+    return currentValue * this.valueToDivide;
+  }
+}
+
+// Chaining commands classes into a class
+class AddThenMultiplyCommand {
+
+  constructor(valueToAdd, valueToMultiply) {
+    this.addCommand = new AddCommand(valueToAdd)
+    this.multiplyCommand = new MultiplyCommand(valueToMultiply)
+  }
+
+  execute (currentValue) {
+    const newValue = this.addCommand.execute(currentValue)
+    return this.multiplyCommand.execute(newValue)
+  }
+
+  undo (currentValue) {
+    const newValue= this.multiplyCommand.undo(currentValue)
+    return this.addCommand.undo(newValue)
+  }
+}
